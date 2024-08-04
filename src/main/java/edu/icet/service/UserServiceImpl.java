78601f9985c0af4df8330241c4e17ac49939f85c
Tharindu-Chamara <tchamara59@gmail.com>
2024-08-05 00:17:16 +0530

    repository done

--- src/main/java/edu/icet/service/UserServiceImpl.java
+++ src/main/java/edu/icet/service/UserServiceImpl.java
@@ -1,10 +1,19 @@
 package edu.icet.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import edu.icet.dto.User;
+import edu.icet.entity.UserEntity;
+import edu.icet.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+
+    final ObjectMapper mapper;
+    final UserRepository userRepository;
+
     @Override
     public String login(User user) {
         return "";
@@ -12,6 +21,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public String register(User user1) {
-        return "";
+       userRepository.save(mapper.convertValue(user1, UserEntity.class));
+       return "OK";
     }
+
 }
